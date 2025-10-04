7874e48a89173ecb32b91433ec26543f524f1002
Cheng Dong <dong0085@algonquinlive.com>
2025-10-04 16:40:10 -0400

    make links open on a new page

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,34 +2,48 @@ import { useEffect, useRef } from 'react';
 import MessageBubble from './MessageBubble';
 import TypingBubble from './TypingBubble';
 import ChatInput from './ChatInput';
-import {useChat} from '../../hooks/useChat';
+import { useChat } from '../../hooks/useChat';
 
 export default function Chat() {
-  const {messages, articles, makePrompt, fetchArticles, error, loading, } = useChat()
-	const bottomRef = useRef(null);
+  const { messages, articles, makePrompt, fetchArticles, error, loading } =
+    useChat();
+  const bottomRef = useRef(null);
 
-	useEffect(() => {
-		bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
-	}, [messages, loading]);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages, loading]);
 
-  if (error) return <div><p>Something Wrong</p></div>
+  if (error)
+    return (
+      <div>
+        <p>Something Wrong</p>
+      </div>
+    );
 
-	return (
-		<section className="rounded-2xl border border-[#2A3238] bg-[#1C2026] p-3 h-[72vh] flex flex-col">
-			<ul className="flex-1 overflow-y-auto space-y-0.5 pr-1 flex flex-col justify-end">
-				{messages.map((m, i) => (
-          m.role !== 'system' && <MessageBubble key={i} msg={m} />
-				))}
-				{loading && <TypingBubble />}
-				<div ref={bottomRef} />
-        {!!articles.length && articles.map(a =>
-          <li><a href={`https://doi.org/${a.doi}`}>{a.title}</a></li>
+  return (
+    <section className="rounded-2xl border border-[#2A3238] bg-[#1C2026] p-3 h-[72vh] flex flex-col">
+      <ul className="flex-1 overflow-y-auto space-y-0.5 pr-1 flex flex-col justify-end">
+        {messages.map(
+          (m, i) => m.role !== 'system' && <MessageBubble key={i} msg={m} />
         )}
-			</ul>
-			<div className="pt-3">
-				<ChatInput onSend={makePrompt} disabled={loading} />
-			</div>
+        {loading && <TypingBubble />}
+        <div ref={bottomRef} />
+        {!!articles.length &&
+          articles.map((a) => (
+            <li>
+              <a
+                href={`https://doi.org/${a.doi}`}
+                target="_blank"
+                rel="noopener noreferrer">
+                {a.title}
+              </a>
+            </li>
+          ))}
+      </ul>
+      <div className="pt-3">
+        <ChatInput onSend={makePrompt} disabled={loading} />
+      </div>
       <button onClick={fetchArticles}>Get Articles Now!</button>
-		</section>
-	);
+    </section>
+  );
 }
